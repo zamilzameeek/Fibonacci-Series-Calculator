a3cbd464b6f73a5014bc1bbb361cd05a79b0162f
Zamil Zameek <zamilzameek@gmail.com>
2026-01-21 11:53:53 +0200

    Update FSM_TOP_LEVEL.v

--- src/FSM_TOP_LEVEL.v
+++ src/FSM_TOP_LEVEL.v
@@ -10,44 +10,10 @@ module FSM_TOP_LEVEL (DONE, DATA, START, CLK, RST, COUNT);
     wire [SIZE-3:0] wrt_addr, rd_addr1, rd_addr2;
     wire wrt_en, clk_internal, load_data, ZERO_FLAG;
 
-    // Core Datapath
-    fibo_datapath F2 (
-        .data(Data_in), 
-        .zero_flag(ZERO_FLAG), 
-        .count(COUNT), 
-        .wrt_addr(wrt_addr), 
-        .wrt_en(wrt_en), 
-        .clk(clk_internal), 
-        .load_data(load_data), 
-        .rd_addr1(rd_addr1), 
-        .rd_addr2(rd_addr2), 
-        .alu_opcode(alu_opcode)
-    );
-
-    // Controller Unit
-    fibo_fsm F1 (
-        .START(START), 
-        .CLK(CLK), 
-        .ZERO_FLAG(ZERO_FLAG), 
-        .RST(RST), 
-        .DONE(DONE), 
-        .alu_opcode(alu_opcode), 
-        .rd_addr1(rd_addr1), 
-        .rd_addr2(rd_addr2), 
-        .wrt_addr(wrt_addr), 
-        .wrt_en(wrt_en), 
-        .clk_out(clk_internal), 
-        .load_data(load_data)
-    );
-
-    // System Memory/Register
-    memory F3 (
-        .Incounter(COUNT), 
-        .Outcounter(Outcounter), 
-        .Data_in(Data_in), 
-        .Data_out(DATA), 
-        .Clk(CLK)
-    );
+    fibo_fsm F1(START, CLK, ZERO_FLAG, RST, DONE, alu_opcode, rd_addr1, rd_addr2, wrt_addr, wrt_en, clk, load_data);
+    fibo_datapath F2(Data_in, ZERO_FLAG, COUNT, wrt_addr, wrt_en, clk, load_data, rd_addr1, rd_addr2, alu_opcode);
+    memory F3(COUNT, Outcounter, Data_in, DATA, CLK);
 
 
 endmodule
+
