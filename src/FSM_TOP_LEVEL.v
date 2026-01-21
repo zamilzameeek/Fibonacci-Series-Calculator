4c111041145e7a111cee294da56fff8011b60bdf
Zamil Zameek <zamilzameek@gmail.com>
2026-01-21 11:50:33 +0200

    Update FSM_TOP_LEVEL.v

--- src/FSM_TOP_LEVEL.v
+++ src/FSM_TOP_LEVEL.v
@@ -1,13 +1,13 @@
 module FSM_TOP_LEVEL (DONE, DATA, START, CLK, RST, COUNT);
-    parameter size = 4;
-    output [size-1:0] DATA;
+    parameter SIZE = 4;
+    output [SIZE-1:0] DATA;
     output DONE;
-    input [size-1:0] COUNT;
+    input [SIZE-1:0] COUNT;
     input START, CLK, RST;
 
-    wire [size-1:0] Outcounter, Data_in;
-    wire [2:0] alu_opcode;
-    wire [1:0] wrt_addr, rd_addr1, rd_addr2;
+    wire [SIZE-1:0] Outcounter, Data_in;
+    wire [SIZE-2:0] alu_opcode;
+    wire [SIZE-3:0] wrt_addr, rd_addr1, rd_addr2;
     wire wrt_en, clk_internal, load_data, ZERO_FLAG;
 
     // Core Datapath
@@ -48,5 +48,6 @@ module FSM_TOP_LEVEL (DONE, DATA, START, CLK, RST, COUNT);
         .Data_out(DATA), 
         .Clk(CLK)
     );
+
 
 endmodule
